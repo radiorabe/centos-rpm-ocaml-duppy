fcad992346f19375397e5643f70d666d9adc4aaa
Lucas Bickel <hairmare@purplehaze.ch>
2020-12-04 15:30:44 +0100

    fix: no explixit dep but disable epel

--- .travis/rpm.sh
+++ .travis/rpm.sh
@@ -6,6 +6,8 @@ set -xe
 
 curl -o /etc/yum.repos.d/ocaml.repo "https://download.opensuse.org/repositories/home:/radiorabe:/liquidsoap:/ocaml/CentOS_8/home:radiorabe:liquidsoap:ocaml.repo"
 
+dnf config-manager --set-disabled epel
+
 chown root:root ocaml-duppy.spec
 
 build-rpm-package.sh ocaml-duppy.spec
